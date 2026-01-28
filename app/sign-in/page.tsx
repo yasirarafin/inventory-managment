34b7be960f01f37f1716475fcb2cecb57966d76b
Yasir Arafin <yasirarafintonmoy@gmail.com>
2026-01-29 02:34:02 +0600

    no dark or light

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,11 +1,14 @@
 import { SignIn } from "@stackframe/stack";
+import Link from "next/link";
 
 export default function SignInPage() {
 
 
     return (
         <div className="min-h-screen flex items-center justify-center">
-           <div><SignIn /></div>
+           <div><SignIn />
+           <Link href="/" className="text-blue-500 hover:underline mt-4 block text-center">Back to Home</Link>  
+           </div>
         </div>
 
 
