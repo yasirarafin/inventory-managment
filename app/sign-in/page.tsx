faa65c9caf40595913078d970ce1e38c4cd3aacf
Yasir Arafin <yasirarafintonmoy@gmail.com>
2026-01-29 02:28:50 +0600

    signin+prisma

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,15 @@
+import { SignIn } from "@stackframe/stack";
+
+export default function SignInPage() {
+
+
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+           <div><SignIn /></div>
+        </div>
+
+
+    )
+    
+
+}
